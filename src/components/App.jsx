1aa26b3217d547df0f99023c5a0d026c2a787793
Priscilla Egberi <100158570+Priscilla-Egberi@users.noreply.github.com>
2022-09-21 18:57:38 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+// import Items from "./items";
+
+function App() {
+  const [item, items] = useState([]);
+  const [text, setText] = useState("");
+
+  function getText(event) {
+    setText(event.target.value);
+  }
+
+  function addText() {
+    items((item) => [...item, text]);
+  }
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={getText} value={text} />
+        <button onClick={addText}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {item.map((x) => (
+            <li>{x}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
